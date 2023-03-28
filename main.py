1617543aaf78ee2785676b6ab77e5c9920413836
sanderVatne <79106278+sanderVatne@users.noreply.github.com>
2023-03-28 09:33:01 +0200

    added get smarethouse

--- main.py
+++ main.py
@@ -30,5 +30,10 @@ def root():
     return {"message": "Welcome to SmartHouse Cloud REST API - Powered by FastAPI"}
 
 
+@app.get("/smarthouse/")
+def read_smarthouse():
+    return smart_house
+
+
 if __name__ == '__main__':
     uvicorn.run(app, host="127.0.0.1", port=8000)
